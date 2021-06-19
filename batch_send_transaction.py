0bdb0474298c3e27a80e14dc7c838b7ee46c5a38
leoliao <me@liaozhuochun.com>
2021-06-19 20:55:20 +0800

    add batch value.

--- batch_send_transaction.py
+++ batch_send_transaction.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser(description='Swarm Bee Batch transaction sender
 parser.add_argument('--input', '-i', help='输入csv文件', default="address_key.csv" )
 parser.add_argument('--rpc', '-r', help='节点rpc', default='http://172.16.4.40:19000')
 parser.add_argument('--key', '-k', help='转出eth和bzz的私钥')
+parser.add_argument('--bzz', '-bzz', help='转出bzz数量', default= 1, type=float)
+parser.add_argument('--eth', '-eth', help='转出eth数量', default=0.1, type=float)
 args = parser.parse_args()
 
 w3 = Web3(Web3.HTTPProvider(args.rpc))
@@ -18,8 +20,8 @@ if w3.isConnected() is False:
     exit()
 
 chain_id = 5 #goerli
-eth_initail_value = 0.1
-gbzz_value = 1
+eth_initail_value = args.eth
+gbzz_value = args.bzz
 gbzz_contract_addr = "0x2aC3c1d3e24b45c6C310534Bc2Dd84B5ed576335"
 # from_private_key = "0x4429bb66e3a651652669effece888b05fe05022565158450e0260fda7cec6113"
 from_private_key = args.key
@@ -31,7 +33,7 @@ startNonce = w3.eth.getTransactionCount(from_addr)
 #estimate gas price
 gasPriceWei = w3.eth.gasPrice
 gasPrice = w3.fromWei(gasPriceWei + 10, 'gwei')  #a larger gas wei
-print("current gasPrice:%d" % (gasPrice))
+print("current gasPrice:%d eth_value:%f bzz_value:%f" % (gasPrice, eth_initail_value, gbzz_value))
 
 #gbzz conrtact
 gbzz_contract = w3.eth.contract(address=gbzz_contract_addr, abi=EIP20_ABI)
@@ -43,43 +45,45 @@ with open(args.input, newline='') as csvfile:
     for row in reader:
         to_addr = row[0]
 
+        if gbzz_value > 0:
         #1. Send gbzz
-        gbzz_txn = gbzz_contract.functions.transfer(
-            to_addr,
-            gbzz_value * pow(10, 16),
-        ).buildTransaction({
-            'chainId': chain_id,
-            'gas': 78394,
-            'gasPrice': w3.toWei(gasPrice, 'gwei'),
-            'nonce': startNonce + i,
-        })
+            gbzz_txn = gbzz_contract.functions.transfer(
+                to_addr,
+                gbzz_value * pow(10, 16),
+            ).buildTransaction({
+                'chainId': chain_id,
+                'gas': 78394,
+                'gasPrice': w3.toWei(gasPrice, 'gwei'),
+                'nonce': startNonce + i,
+            })
 
-        signed_txn = w3.eth.account.signTransaction(gbzz_txn, from_private_key)
+            signed_txn = w3.eth.account.signTransaction(gbzz_txn, from_private_key)
 
-        tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+            tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-        tx_hex = w3.toHex(tx)
+            tx_hex = w3.toHex(tx)
 
-        print("send from:%s to:%s gbzz value:%d hash:%s" %(from_addr, to_addr, gbzz_value, tx_hex))
+            print("send from:%s to:%s gbzz value:%d hash:%s" %(from_addr, to_addr, gbzz_value, tx_hex))
 
-        i = i + 1
+            i = i + 1
 
         #2. Send geth
-        eth_transaction = {
-        'from': from_addr,
-        'to': to_addr,
-        'value': w3.toWei(0.1, "ether"),
-        'gas': 21000,
-        'gasPrice': w3.toWei(gasPrice, 'gwei'),
-        'nonce': startNonce + i,
-        'chainId': chain_id}
+        if eth_initail_value > 0:
+            eth_transaction = {
+            'from': from_addr,
+            'to': to_addr,
+            'value': w3.toWei(0.1, "ether"),
+            'gas': 21000,
+            'gasPrice': w3.toWei(gasPrice, 'gwei'),
+            'nonce': startNonce + i,
+            'chainId': chain_id}
 
-        signed_txn = w3.eth.account.signTransaction(eth_transaction, from_private_key)
+            signed_txn = w3.eth.account.signTransaction(eth_transaction, from_private_key)
 
-        tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+            tx = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 
-        tx_hex = w3.toHex(tx)
+            tx_hex = w3.toHex(tx)
 
-        print("send from:%s to:%s eth value:%f hash:%s" % (from_addr, to_addr, eth_initail_value, tx_hex))
+            print("send from:%s to:%s eth value:%f hash:%s" % (from_addr, to_addr, eth_initail_value, tx_hex))
 
-        i = i + 1
+            i = i + 1
